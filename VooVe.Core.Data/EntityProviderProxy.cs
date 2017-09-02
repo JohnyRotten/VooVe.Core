b6b1181b38641ad6ec29a3ca6cd34a8f60626880
Petrunkin Oleg <mr.alegon@yandex.ru>
2017-09-02 16:47:54 +0300

    Add proxy for IEntityProvider

--- VooVe.Core.Data/EntityProviderProxy.cs
+++ VooVe.Core.Data/EntityProviderProxy.cs
@@ -1,10 +1,33 @@
-﻿namespace VooVe.Core.Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace VooVe.Core.Data
 {
     public class EntityProviderProxy : IEntityProvider
     {
+        private readonly IEntityProvider _provider;
+
+        private readonly IDictionary<Type, Func<object>> _proxyMap = new Dictionary<Type, Func<object>>();
+
+        public EntityProviderProxy(IEntityProvider provider)
+        {
+            _provider = provider;
+        }
+        
         public IEntitySet<TEntity> Set<TEntity>()
         {
-            throw new System.NotImplementedException();
+            var type = typeof(IEntitySet<TEntity>);
+            IEntitySet<TEntity> result;
+            if (!_proxyMap.ContainsKey(type))
+            {
+                result = new EntitySetProxy<TEntity>(_provider.Set<TEntity>());
+                _proxyMap[type] = () => result;
+            }
+            else
+            {
+                result = (IEntitySet<TEntity>)_proxyMap[type]();
+            }
+            return result;
         }
     }
 }
---
+++ VooVe.Core.Data/EntitySetProxy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VooVe.Core.Data
+{
+    internal class EntitySetProxy<TEntity> : IEntitySet<TEntity>
+    {
+        private readonly IEntitySet<TEntity> _entitySet;
+        private IList<TEntity> _items = new List<TEntity>();
+        private const int DefaultRefreshableTime = 10;
+        private DateTime UpdatedTime { get; set; } = DateTime.Now;
+        public TimeSpan RefreshableTime { get; set; } = TimeSpan.FromMinutes(DefaultRefreshableTime);
+
+        protected IList<TEntity> Items
+        {
+            get
+            {
+                if ((DateTime.Now - UpdatedTime) > RefreshableTime || _items == null)
+                {
+                    Refresh();
+                }
+                return _items;
+            }
+        }
+
+        public EntitySetProxy(IEntitySet<TEntity> entitySet)
+        {
+            _entitySet = entitySet;
+        }
+
+        public IEnumerable<TEntity> Get(Predicate<TEntity> condition)
+        {
+            return Items.Where(e => condition(e));
+        }
+
+        public void Add(TEntity entity)
+        {
+            _entitySet.Add(entity);
+            Refresh();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _entitySet.Update(entity);
+            Refresh();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _entitySet.Remove(entity);
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            _items = _entitySet.Get(_ => true).ToList();
+            UpdatedTime = DateTime.Now;
+        }
+
+    }
+}
